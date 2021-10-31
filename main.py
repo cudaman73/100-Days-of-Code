f714630cf54b30921e5fd0a391698a208f4dae6c
cudaman73 <dale.evans.3@gmail.com>
2021-10-30 20:29:23 -0500

    Add docstrings

--- main.py
+++ main.py
@@ -33,20 +33,24 @@ resources = {
 
 
 def inventory_report():
+    """Prints an on-hand inventory report"""
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}")
     print(f"Coffee: {resources['coffee']}g")
     print(f"Money: ${resources['money']}")
 
+
 def check_inventory(drink):
+    """Checks to see if there is sufficient inventory to make drink, and returns success or failure"""
     for key, value in MENU[drink]["ingredients"].items():
         if value > resources[key]:
             print("We don't have enough", str(key), "at the moment, sorry.")
-            return "fail"
+            return "failure"
     return "success"
 
 
 def handle_money(drink):
+    """This calculates how much the user pays and makes changes or refunds. Returns success or failure"""
     print("Your drink will cost $" + str(MENU[drink]["cost"]) + ".")
     q = int(input("Please input how many quarters you'll use:"))
     d = int(input("Please input how many dimes you'll use:"))
@@ -60,12 +64,13 @@ def handle_money(drink):
         print("Here's $" + str(round(total - MENU[drink]["cost"], 2)) + " in change.")
     elif total < MENU[drink]["cost"]:
         print("You gave me $" + str(total) + ", which isn't enough, starting over")
-        return "fail"
+        return "failure"
     resources["money"] += MENU[drink]["cost"]
     return "success"
 
 
 def make_coffee(drink):
+    """removes coffee ingredients from inventory"""
     for key, value in MENU[drink]["ingredients"].items():
         resources[key] -= value
 
