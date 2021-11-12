15a2f7ac80be8396cd01fa6269646164ed780421
cudaman73 <dale.evans.3@gmail.com>
2021-11-11 19:40:13 -0600

    Day 27 - Adds Nato Alphabet project - which takes user input and converts it into a list containing the nato phonetic versions of each letter.

---
+++ 26 - NATO Alphabet Project/main.py
@@ -0,0 +1,33 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+nato_dict = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+# Create a dictionary in this format:
+# Iterate over csv in form letter: code to create new dictionary with values
+nato_alphabet = {row.letter: row.code for (index, row) in nato_dict.iterrows()}
+
+# 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Give me a word, I will spell it out phonetically:").upper()
+nato_array = [nato_alphabet[letter] for letter in word]
+
+print(nato_array)
