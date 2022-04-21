37eeb20562a5340f6cca89e762cd5ed40d238706
cudaman73 <dale.evans.3@gmail.com>
2022-04-20 22:00:43 -0500

    Day 30 - Refactors day 26 to handle KeyErrors in input. Refactors Day 29 to handle storing password data in JSON format, and adds some error handling to password manager.

--- 26 - NATO Alphabet Project/main.py
+++ 26 - NATO Alphabet Project/main.py
@@ -1,21 +1,21 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
+# import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
@@ -26,8 +26,20 @@ nato_dict = pandas.read_csv("nato_phonetic_alphabet.csv")
 # Iterate over csv in form letter: code to create new dictionary with values
 nato_alphabet = {row.letter: row.code for (index, row) in nato_dict.iterrows()}
 
-# 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Give me a word, I will spell it out phonetically:").upper()
-nato_array = [nato_alphabet[letter] for letter in word]
+
+# Create a list of the phonetic code words from a word that the user inputs.
+# Day 30 - we add KeyError catching for when users enter invalid input. An
+# alternative approach would be to use function recursion - if we encased our
+# try/except/else functionality in a function, we can call the function again
+# during the exception, which will loop until there is no KeyError.
+
+while True:
+    try:
+        word = input("Give me a word, I will spell it out phonetically:").upper()
+        nato_array = [nato_alphabet[letter] for letter in word]
+        break
+    except KeyError:
+        print("Sorry, please only input letters.")
 
 print(nato_array)
+
---
+++ 30 - Error Handling/main.py
@@ -0,0 +1,148 @@
+# This is not a real password manager - all passwords stored in plaintext, this is not safe lmao
+
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import json
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_password():
+    """Generates a random password with 8-10 letters, 2 - 4 numbers, 2 - 4 symbols, then
+    enters it into the password form, and copies the password to the clipboard"""
+    # Angela created 3 separate lists using list comprehension and added them together,
+    # but I like doing it this way instead, it makes more sense to me, even if logically,
+    # they are identical methods
+    password_list = []
+    password_list += [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_text.delete(0, END)
+    password_text.insert(0, password)
+    # a clipboard-friendlier way would be probably to use pyperclip or some other pip module,
+    # but I made the decision to do it with raw Tk commands to simplify the code. This code
+    # clears the clipboard first, then adds the text.
+    window.clipboard_clear()
+    window.clipboard_append(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+# Day 30 refactor - we are moving from stupid basic text storage to storing data
+# in JSON formatting - since JSON is the superior way to store keys and values.
+# adjusted input validation to simplify JSON object creation and improve readability
+
+def save_password():
+    website = website_text.get()
+    password = password_text.get()
+    email = email_text.get()
+    """This should save the current values of the password manager in passwords.txt"""
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showwarning(title="Oops", message="Double check the website/password fields")
+    else:
+        json_package = {website: {"email": email, "password": password}}
+        try:
+            with open("passwords.json", "r") as file:
+                json_file = json.load(file)
+        except (ValueError, FileNotFoundError):
+            # if file doesn't exist/is empty, we create new file with entry
+            with open("passwords.json", "w") as file:
+                json.dump(json_package, file, indent=4)
+        else:
+            json_file.update(json_package)
+
+            # write updated json to disk
+            with open("passwords.json", "w") as file:
+                json.dump(json_file, file, indent=4)
+
+        # reset app for next input
+        website_text.delete(0, END)
+        password_text.delete(0, END)
+        website_text.focus()
+
+
+# I wrote this ahead of the video with some minor googling, not realizing there was a messagebox module.
+# I am going to leave it in the code rather than rewrite things so that hopefully, if I come back to view
+# this later, I will remember there are multiple ways to do things - although some ways are harder than
+# using a built in module, haha.
+#
+# def open_popup():
+#     top = Toplevel(window)
+#     top.geometry("225x100")
+#     top.title(f"{website_text.get()}")
+#     l = Label(top, text=f"Email/Username: {email_text.get()}\nPassword: {password_text.get()}\nIs this okay?")
+#     l.grid(row=0, column=0, columnspan=2, padx=20)
+#
+#     b = Button(top, text="No", command=top.destroy)
+#     b.grid(row=1, column=0)
+#
+#     b2 = Button(top, text="Yes", command=lambda: [save_password(), top.destroy()])
+#     b2.grid(row=1, column=1)
+
+
+# -------------------------- SEARCH PASSWORD -------------------------- #
+def search_passwords():
+    website = website_text.get()
+    try:
+        with open("passwords.json", "r") as file:
+            json_file = json.load(file)
+            data = json_file[website]
+            email = data['email']
+            password = data['password']
+            messagebox.showinfo(title=website, message=f"email: {email}\npassword: {password}")
+    except KeyError:
+        messagebox.showinfo(title="Error", message=f"an entry with {website} does not exist!")
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No password file found.")
+
+
+# -----------------------------  UI SETUP ----------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+logo_image = PhotoImage(file="logo.png")
+
+logo = Canvas(height=200, width=200)
+logo.create_image(100, 100, image=logo_image)
+logo.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_text = Entry(width=33)
+website_text.grid(row=1, column=1)
+website_text.focus()
+
+search_button = Button(text="Search", command=search_passwords)
+search_button.grid(row=1, column=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+email_text = Entry(width=52)
+email_text.grid(row=2, column=1, columnspan=2)
+email_text.insert(0, "fake@email.com")
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+password_text = Entry(width=33)
+password_text.grid(row=3, column=1)
+
+password_button = Button(text="Generate Password", command=generate_password)
+password_button.grid(row=3, column=2, pady=(0, 2))
+
+add_button = Button(text="Add", width=44, command=save_password)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
