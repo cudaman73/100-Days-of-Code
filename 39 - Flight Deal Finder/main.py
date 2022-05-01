22c708fefa5eff79a48994393f4c910bc644b3ec
cudaman73 <dale.evans.3@gmail.com>
2022-05-01 00:15:59 -0500

    Day 39 - switches flyFrom and cityTo so that notification_manager.py sends the correct data in the correct order.

--- 39 - Flight Deal Finder/main.py
+++ 39 - Flight Deal Finder/main.py
@@ -14,8 +14,8 @@ for x in data.dest_cities:
         if flight_data.is_flight_cheaper(flight["price"], data.lowest_prices[x]):
             notify_manager.notify(
                 flight['cityFrom'],
-                flight['flyFrom'],
                 flight['cityTo'],
+                flight['flyFrom'],
                 flight['flyTo'],
                 flight['route'][0]['local_departure'][0:10],
                 flight['route'][-1]['local_departure'][0:10],
