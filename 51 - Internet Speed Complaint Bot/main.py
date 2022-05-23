01bf74c4e5f2500790d83106e619574397c0cec7
Dale Evans <dale.evans.3@gmail.com>
2022-05-23 13:26:27 -0500

    51 - Adds tweet_at_provider to complain when speeds are slow!

--- 51 - Internet Speed Complaint Bot/main.py
+++ 51 - Internet Speed Complaint Bot/main.py
@@ -2,6 +2,7 @@ from config import *
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -14,19 +15,30 @@ class InternetSpeedTwitterBot:
     def get_internet_speed(self):
         self.driver.get('https://www.speedtest.net/')
         self.driver.find_element(By.CLASS_NAME, "js-start-test").click()
-        time.sleep(60)
+        time.sleep(45)
         self.down = self.driver.find_element(By.CLASS_NAME, "download-speed").text
         self.up = self.driver.find_element(By.CLASS_NAME, "upload-speed").text
         print(f"down: {self.down}")
         print(f"up: {self.up}")
 
     def tweet_at_provider(self, up, down):
-        pass
+        message = f"Hey Spectrum, why is my internet speed currently {self.down} down/{self.up} up when I pay for " \
+                  f"{down}/{up}? "
+        self.driver.get("https://twitter.com/i/flow/login")
+        # Have to sleep a bit here because the DOM isn't immediately interactible.
+        time.sleep(3)
+
+        # input username and password
+        self.driver.find_element(By.CLASS_NAME, "r-30o5oe").send_keys(TWITTER_USER, Keys.ENTER)
+        time.sleep(1)
+        self.driver.find_element(By.CLASS_NAME, "r-homxoj").send_keys(TWITTER_PASS, Keys.ENTER)
+        # wait for page to load once we login, then send angry tweet!
+        time.sleep(5)
+        self.driver.find_element(By.CLASS_NAME, "public-DraftEditor-content").send_keys(message)
+        self.driver.find_element(By.CSS_SELECTOR, "div[data-testid='tweetButtonInline']").click()
 
 
 complainer = InternetSpeedTwitterBot()
 
-complainer.get_internet_speed()
-
-# if complainer.up < PROMISED_UP or complainer.down < PROMISED_DOWN:
-#     complainer.tweet_at_provider(PROMISED_UP, PROMISED_DOWN)
+if complainer.up < PROMISED_UP * .9 or complainer.down < PROMISED_DOWN * .9:
+    complainer.tweet_at_provider(PROMISED_UP, PROMISED_DOWN)
